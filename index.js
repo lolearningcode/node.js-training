6444d713e2d67b80806e8c6d60a1c49464aa227d
Lo <lolearningcode@gmail.com>
2019-12-06 01:37:23 -0800

    added functionality to parse the JSON data to placeholders. Having issues with the organic part on the app but will fix later.

--- index.js
+++ index.js
@@ -38,16 +38,63 @@ const url = require('url');
 
 ////////////////////////////////////////////////
 // SERVER
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC}/g, 'not-organic');
+
+    return output;
+}
+
+//Reading the JSON from the API in the dev data folder-----BLOCKING Sync
+const tempOverview = fs.readFileSync(
+    `${__dirname}/templates/template-overview.html`,
+    'utf-8'
+  );
+const tempCard = fs.readFileSync(
+    `${__dirname}/templates/template-card.html`,
+    'utf-8'
+  );
+const tempProduct = fs.readFileSync(
+    `${__dirname}/templates/template-product.html`,
+    'utf-8'
+  );
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObject = JSON.parse(data);
 
 //Created server that will hold the Hello from server
 const server = http.createServer((req, res) => {
     const pathName = req.url;
+    //Overview Page
+    if (pathName === '/' || pathName === '/overview') {
 
-    if (pathName === '/' || pathName === '\overview') {
-        res.end('This is the OVERVIEW!!!');
+        res.writeHead(200, {'Content-type': 'text/html'});
+        //returns an array and replaces each placeholder(el) with a new object(tempcard)
+        const cardsHtml = dataObject.map(el => replaceTemplate(tempCard, el)).join('');
+
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        // console.log(product.organic);
+        res.end(output);
+        //Product Page
     } else if (pathName === '/product'){
         res.end('This is the product!!!');
+        //API
+    } else if (pathName === '/api') {
+        
+        // console.log(productData);
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.end(data);
+        
     } else {
+        //NOT FOUND
         res.writeHead(404, {
             'Content-type': 'text/html',
             'my-own-header': 'hello-world'
